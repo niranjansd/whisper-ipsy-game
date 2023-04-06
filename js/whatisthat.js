a2335a1f6f1c337a2807b06dd3a82e66ffec8df4
niranjansd <niranjan.sd@gmail.com>
2023-04-06 06:53:11 -0700

    Works but streaming api is slow, not realtime.

--- js/whatisthat.js
+++ js/whatisthat.js
@@ -10,11 +10,12 @@ const correctAudio = document.getElementById('correct-audio');
 Date.prototype.today = function () { 
     return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
 }
-  
 // For the time now
 Date.prototype.timeNow = function () {
     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
 }
+RECORD_LIMIT = 4;
+window.answer = ''  
   
 
 const buttonData = [{imgSrc: 'objects/prod/heart.png',   
@@ -42,24 +43,25 @@ whatisthatbutton.addEventListener('click', () => {
     const randomIndex = Math.floor(Math.random() * buttonData.length);
     const randomButtonData = buttonData[randomIndex];
     const correctAnswer = randomButtonData.answer;
+    window.correctAnswer = correctAnswer.toLowerCase();
     // display the image in the gameboard
     gameboard.innerHTML = `<div id="gameimg"><div id="detected" contenteditable="true" hidden></div><img src="${randomButtonData.imgSrc}" width="100" height="100" alt="${randomButtonData.answer}"></div>`;
 
-    function startRecording() {
-        console.log('recording started');
+    function startRecording(i) {
+        console.log('recording started', i);
         navigator.mediaDevices.getUserMedia({ audio: true })
         .then((stream) => {
             recorder = new MediaRecorder(stream);
             recorder.start();
-            setTimeout(stopRecording, 10000, recorder);
+            setTimeout(stopRecording, 10000, recorder, i);
         });
     };
-    function stopRecording(recorder) {
+    function stopRecording(recorder, i) {
         console.log('recording stopped');
         recorder.stop();
-        processRecording(recorder);
+        processRecording(recorder, i);
     };
-    const processRecording = (recorder) => {
+    const processRecording = (recorder, i) => {
         var datetime = new Date().today() + "_" + new Date().timeNow();
         var datetime = datetime.replace(/:/g, "-").replace("/", "-").replace("/", "-");
         var filename = 'recording_'+datetime+'.wav'
@@ -73,21 +75,37 @@ whatisthatbutton.addEventListener('click', () => {
                 a.download = filename;
                 a.click();
                 console.log('recording saved');
-                loader.style.display = 'block';
-                setTimeout(send_answer, 1000,
-                           "C:\\Users\\niran\\Downloads\\"+filename);
-                // send_answer("C:\\Users\\niran\\Downloads\\"+filename);
-                loader.style.display = 'none';
-                console.log('ans', $("#detected").html(),
-                            'correct', correctAnswer);
+                // loader.style.display = 'block';
+                setTimeout(send_answer, 500,
+                            "C:\\Users\\niran\\Downloads\\"+filename,
+                            );
+                // req = send_answer("C:\\Users\\niran\\Downloads\\"+filename);
+                // correctness = check_answer(req, correctAnswer);
+                // loader.style.display = 'none';
+                // console.log('ans', $("#detected").html(),
+                //             'correct', correctAnswer);
                 // // delete the file
                 // delete_file("C:\\Users\\niran\\Downloads\\"+filename);
             });
         };
         recorder = null;
-        return recorder;
+        console.log('window.answer', window.answer,
+                    'window.correctAnswer', window.correctAnswer);
+        // make answer lowercase
+        window.answer = window.answer.toLowerCase();
+        //  check if correct answer is a substring of the answer
+        //  if yes, stop recording
+        //  if not, start recording again
+        if (window.answer.includes(window.correctAnswer)) {
+            console.log('correct', i);
+            // correctAudio.play();
+        } else {
+            console.log('incorrect', i);
+            // failureAudio.play();
+            startRecording(i+1);
+        };
     };
-    startRecording();
+    startRecording(0);
 });
 
 
@@ -109,12 +127,33 @@ function send_answer(filename) {
         }),
         dataType: "json",
         success: function (results) {
-            console.log(results);
-            // Add results to the page in the transcript div.
-            $("#detected").html(results.text);
+            console.log(results['transcript']);
+            if (results['transcript']) {
+                console.log("No answer detected.");
+                return;
+            } else {
+                console.log("Answer detected.");
+                window.answer = results['transcript']
+            };
+            // return results['transcript'];
+            // return results.text;
+            // check_answer(results.text, correctAnswer);
         }
     });
+    return req;
 };
+
+check_answer = function (ans, correct) {
+    if (ans == correct) {
+        console.log('correct', ans, correct);
+        successAudio.play();
+    } else {
+        console.log('incorrect', ans, correct);
+        // failureAudio.play();
+    };
+    return ans == correct;
+};
+        
 
 function delete_file(filename) {
     console.log("Deleting file: " + filename);
